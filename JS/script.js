5ddf8ced979afa6120279772d70c41b502a6112a
Marealta-J <pyxalalova.yulya666@gmail.com>
2023-03-16 18:31:15 +0600

    addedControlButton

--- JS/script.js
+++ JS/script.js
@@ -403,3 +403,24 @@ window.addEventListener('keydown', function (e){
    
    
 })
+
+
+
+/* Всплывающее окно "Управление" */
+function funcDropDown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Закрытие окна при клике за его пределами
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
